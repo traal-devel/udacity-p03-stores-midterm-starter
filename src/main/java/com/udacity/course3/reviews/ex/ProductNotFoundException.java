75377733eb372fbd27b5f0f783f61121405f1b5c
devel@traal.ch <devel@traal.ch>
2019-12-13 21:43:44 +0100

    style: Change java syntax format.

--- src/main/java/com/udacity/course3/reviews/ex/ProductNotFoundException.java
+++ src/main/java/com/udacity/course3/reviews/ex/ProductNotFoundException.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
   code = HttpStatus.NOT_FOUND, 
   reason = "Product not found"
 )
-public class ProductNotFoundException 
-       extends RuntimeException {
-
+public class ProductNotFoundException extends RuntimeException {
   
 
   /* constants */
