8de89dd073a36f269190323061602ac598a39a9c
devel@traal.ch <devel@traal.ch>
2019-12-14 15:04:40 +0100

    feat: Add method findByReviewId which uses JPQL.

--- src/main/java/com/udacity/course3/reviews/repository/CommentRepository.java
+++ src/main/java/com/udacity/course3/reviews/repository/CommentRepository.java
@@ -1,6 +1,11 @@
 package com.udacity.course3.reviews.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.udacity.course3.reviews.entity.Comment;
@@ -9,5 +14,9 @@ import com.udacity.course3.reviews.entity.Comment;
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
   /* methods */
-
+  @Query("SELECT c FROM Comment c JOIN c.review r ON r.id = :reviewId ")
+  Optional<List<Comment>> findByReviewId(
+      @Param(value = "reviewId") Integer reviewId
+  );
+  
 }
