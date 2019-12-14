3612923216dfc19c347929b8c5dd49191ab2727e
devel@traal.ch <devel@traal.ch>
2019-12-14 15:04:56 +0100

    feat: Add method findByProductId which uses JPQL.

--- src/main/java/com/udacity/course3/reviews/repository/ReviewRepository.java
+++ src/main/java/com/udacity/course3/reviews/repository/ReviewRepository.java
@@ -1,6 +1,11 @@
 package com.udacity.course3.reviews.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.udacity.course3.reviews.entity.Review;
@@ -9,5 +14,9 @@ import com.udacity.course3.reviews.entity.Review;
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
   /* methods */
+  @Query("SELECT r FROM Review r JOIN r.product p ON p.id = :productId ")
+  Optional<List<Review>> findByProductId(
+      @Param(value = "productId") Integer productId
+  );
 
 }
