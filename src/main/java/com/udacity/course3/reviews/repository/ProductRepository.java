815f8e02c445feb3b7a6de4189c67b8b0ec52614
devel@traal.ch <devel@traal.ch>
2019-12-31 19:25:57 +0100

    style: Change Java-Code-Style.

--- src/main/java/com/udacity/course3/reviews/repository/ProductRepository.java
+++ src/main/java/com/udacity/course3/reviews/repository/ProductRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.udacity.course3.reviews.entity.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Integer>{
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
   /* methods */
 
