b85e32168a3788d70bef51452cdc115c1e16c84b
devel@traal.ch <devel@traal.ch>
2020-01-01 19:16:02 +0100

    feat: Add spring bean MappingJackson2HttpMessageConverter.
    Add a Jackson2ObjectMapperBuilder so (mongo-db) ObjectId is serialized
    to a String (-> REST response).

--- src/main/java/com/udacity/course3/reviews/ReviewsApplication.java
+++ src/main/java/com/udacity/course3/reviews/ReviewsApplication.java
@@ -1,16 +1,14 @@
 package com.udacity.course3.reviews;
 
-import org.modelmapper.ModelMapper;
+import org.bson.types.ObjectId;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -31,19 +29,22 @@ public class ReviewsApplication {
 		SpringApplication.run(ReviewsApplication.class, args);
 	}
 	
+	/**
+	 * Gets the MappingJackson2HttpMessageConverter.
+	 * <p>
+	 * Add a builder so (mongo-db) ObjectId is serialized to a String. 
+	 * </p>
+	 * 
+	 * @return {@link MappingJackson2HttpMessageConverter}
+	 */
 	@Bean
-	public ModelMapper modelMapper() {
-    return new ModelMapper();
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+	  Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+    builder.serializerByType(ObjectId.class, new ToStringSerializer());
+    MappingJackson2HttpMessageConverter converter = 
+        new MappingJackson2HttpMessageConverter(builder.build());
+	 
+	 return converter;
 	}
-	
-//	@Bean
-//	public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-//    ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-//    objectMapper.registerModule(new Jdk8Module());
-//    objectMapper.registerModule(new JavaTimeModule());
-//    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-////      objectMapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
-//    return objectMapper;
-//  }
 	
 }
