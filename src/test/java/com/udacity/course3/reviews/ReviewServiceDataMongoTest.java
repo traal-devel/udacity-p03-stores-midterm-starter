94163164d046d684cb46b1c89a21ec45acb0f9cc
devel@traal.ch <devel@traal.ch>
2020-01-02 12:25:13 +0100

    feat: Add test for calculating average rating via mongo-db (POC).

--- src/test/java/com/udacity/course3/reviews/ReviewServiceDataMongoTest.java
+++ src/test/java/com/udacity/course3/reviews/ReviewServiceDataMongoTest.java
@@ -5,10 +5,15 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
 
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bson.Document;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,20 +22,28 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.udacity.course3.reviews.entity.Product;
+import com.udacity.course3.reviews.data.entity.Product;
+import com.udacity.course3.reviews.data.model.Review;
 import com.udacity.course3.reviews.ex.ProductNotFoundException;
-import com.udacity.course3.reviews.model.Review;
 import com.udacity.course3.reviews.repository.ProductRepository;
 import com.udacity.course3.reviews.repository.ReviewRepository;
 import com.udacity.course3.reviews.service.ReviewService;
 import com.udacity.course3.reviews.util.DummyDataUtil;
 
+
 @RunWith(SpringRunner.class)
 @DataMongoTest
 public class ReviewServiceDataMongoTest {
 
+  
+  /* constants */
+  private static final Logger logger = 
+                    LogManager.getLogger(ReviewServiceDataMongoTest.class);
   
   /* member variables */
   @MockBean
@@ -41,6 +54,9 @@ public class ReviewServiceDataMongoTest {
   
   @Autowired
   private ReviewService     reviewService;
+  
+  @Autowired 
+  private MongoTemplate     mongoTemplate;
   
   
   /* constructors */
@@ -94,6 +110,40 @@ public class ReviewServiceDataMongoTest {
     assertEquals(reviewDummy.getBody(), reviewDB.getBody());
     assertEquals(reviewDummy.getTitle(), reviewDB.getTitle());
 
+  }
+  
+  @Test
+  public void testAverageRating() {
+    Integer productId = 1;
+    int iSize = 20;
+    int sum = 0; 
+    double average = 0;
+    List<Review> reviewList = DummyDataUtil.generateDummyReview(iSize);
+    
+    
+    for (Review review: reviewList) {
+      Review reviewDB = this.reviewService.addReview(productId, review);
+      assertNotNull(reviewDB);
+      assertNotNull(reviewDB.getId());
+      assertNotNull(reviewDB.getRating());
+      
+      sum += reviewDB.getRating();
+    }
+    
+    average = (double)sum / iSize;
+    logger.info("Average rating manually calculated: " + average);
+
+    
+    TypedAggregation<Review> agg = newAggregation(Review.class,
+        group("productId")            
+          .avg("rating").as("avgRating")
+    );
+    AggregationResults<Document> result = mongoTemplate.aggregate(agg, Document.class);
+    List<Document> resultList = result.getMappedResults();
+    double mongoAvgRating = (double)resultList.get(0).get("avgRating");
+    logger.info("Average rating mongodb calculated: " + mongoAvgRating);
+    
+    assertTrue(average == mongoAvgRating);
   }
   
   @TestConfiguration
