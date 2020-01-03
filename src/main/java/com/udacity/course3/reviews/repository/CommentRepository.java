2961beaabe6de378873fb171ef66c0c1cddc5335
devel@traal.ch <devel@traal.ch>
2020-01-03 18:17:30 +0100

    feat: Add jpa repository to master branch again.

---
+++ src/main/java/com/udacity/course3/reviews/repository/CommentRepository.java
@@ -0,0 +1,23 @@
+package com.udacity.course3.reviews.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.udacity.course3.reviews.data.entity.Comment;
+
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+  /* methods */
+  @Query("SELECT c FROM Comment c JOIN c.review r ON r.id = :reviewId ")
+  Optional<List<Comment>> findByReviewId(
+      @Param(value = "reviewId") Integer reviewId
+  );
+  
+}
