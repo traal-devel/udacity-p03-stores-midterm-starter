762af691068e858d7806a84cc4bd315dbaa6823c
devel@traal.ch <devel@traal.ch>
2020-01-03 18:14:36 +0100

    refactor: Use dto classes in the utility to create dummy instances.

--- src/test/java/com/udacity/course3/reviews/util/DummyDataUtil.java
+++ src/test/java/com/udacity/course3/reviews/util/DummyDataUtil.java
@@ -5,9 +5,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.udacity.course3.reviews.data.entity.Product;
-import com.udacity.course3.reviews.data.model.Comment;
-import com.udacity.course3.reviews.data.model.Review;
+import com.udacity.course3.reviews.data.dto.CommentDTO;
+import com.udacity.course3.reviews.data.dto.ProductDTO;
+import com.udacity.course3.reviews.data.dto.ReviewDTO;
 
 public class DummyDataUtil {
 
@@ -22,8 +22,8 @@ public class DummyDataUtil {
 
   
   /* methods */
-  public static Comment generateDummyComment(String suffix) {
-    Comment comment = new Comment();
+  public static CommentDTO generateDummyComment(String suffix) {
+    CommentDTO comment = new CommentDTO();
     
     comment.setContent("TEST_CONTENT_" + suffix);
     comment.setName("TEST_NAME_" + suffix);
@@ -32,24 +32,24 @@ public class DummyDataUtil {
     return comment;
   }
   
-  public static List<Review> generateDummyReview(int n) {
-    List<Review> reviewList = new ArrayList<>(n);
+  public static List<ReviewDTO> generateDummyReview(int n) {
+    List<ReviewDTO> reviewList = new ArrayList<>(n);
     
     for (int i=1; i<n+1; i++) {
-      Review review = new Review();
+      ReviewDTO review = new ReviewDTO();
       review.setAuthor("TEST_AUTHOR_" + i);
       review.setTitle("TEST_TITLE_" + i);
       review.setBody("TEST_BODY_" + i);
       review.setCreatedTime(Timestamp.valueOf(LocalDateTime.now()));
-      review.setRating((int)(Math.random() * 5));
+      review.setRating((int)(Math.random() * 5) + 1);
       reviewList.add(review);
     }
     
     return reviewList;
   }
   
-  public static List<Product> generateDummyProductList(int n) {
-    List<Product> productList = new ArrayList<>(n);
+  public static List<ProductDTO> generateDummyProductList(int n) {
+    List<ProductDTO> productList = new ArrayList<>(n);
     
     for(int i=1; i<n+1; i++) {
       productList.add(DummyDataUtil.generateDummyProduct("_" + i, "Test Description " + i));
@@ -58,12 +58,12 @@ public class DummyDataUtil {
     return productList;
   }
   
-  public static Product generateDummyProduct(
+  public static ProductDTO generateDummyProduct(
       String suffix, 
       String description
   ) {
   
-    Product product = new Product();
+    ProductDTO product = new ProductDTO();
     
     product.setName("Product"  + suffix);
     product.setDescription(description);
